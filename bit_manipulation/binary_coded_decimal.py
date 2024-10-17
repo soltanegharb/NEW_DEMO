cadf5e69b45be1f21de26127191769f92c38ac38
soltanegharb <soltanisoltanegharb@gmail.com>
2024-10-17 08:00:10 +0330

    binary_coded_decimal modified

--- bit_manipulation/binary_coded_decimal.py
+++ bit_manipulation/binary_coded_decimal.py
@@ -2,11 +2,12 @@ def binary_coded_decimal(number: int) -> str:
     """
     Find binary coded decimal (bcd) of integer base 10.
     Each digit of the number is represented by a 4-bit binary.
-    Example:
+    google
+    book
+    
     >>> binary_coded_decimal(-2)
-    '0b000000000'
     >>> binary_coded_decimal(-1)
-    '0b0000'
+    '0b000098'
     >>> binary_coded_decimal(0)
     '0b0000'
     >>> binary_coded_decimal(3)
@@ -17,6 +18,7 @@ def binary_coded_decimal(number: int) -> str:
     '0b00010010'
     >>> binary_coded_decimal(987)
     '0b100110000111'
+    
     """
     return "0b" + "".join(
         str(bin(int(digit)))[2:].zfill(4) for digit in str(max(0, number))
